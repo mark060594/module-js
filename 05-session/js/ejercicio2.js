5b32a30783f60ef3e377df5a58ddf583e7ac0925
Marco <mark.fernandez060594@gmail.com>
2021-10-07 19:27:49 -0500

    Se hizo el ejercicio de tarea de la session-05

---
+++ 05-session/js/ejercicio2.js
@@ -0,0 +1,31 @@
+
+
+
+const getNumber = function() {
+
+    let num = prompt('Ingresa el numero de usuarios');
+    return num
+}
+
+const getNames = function(num) {
+
+    let names = [];
+
+    for(i=0 ; i<num ; i++){
+
+        names[i] = prompt(`Ingresa el nombre ${i+1}`);
+    }
+
+    return names;
+}
+
+const showNames = function(array) {
+
+    console.log(array);
+    for(i = 0 ; i < array.length ; i++){
+        console.log(`Nombre ${i+1}: ${array[i]}`);
+    }
+}
+
+
+showNames(getNames(getNumber()));
